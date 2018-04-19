2745ec38d282308e04905aebf918cadadbb4cf48
Alex <sapalks@gmail.com>
2018-04-19 17:10:54 +0400

    обертка для логгера

---
+++ logger.go
@@ -0,0 +1,71 @@
+//библиотека для логирования
+//обертка для github.com/natefinch/lumberjack
+package logger
+
+import (
+	lumberjack "github.com/natefinch/lumberjack"
+)
+
+//Создание экземпляра логгера на основе конфигурации
+//
+//Пример корфигурации для автоматического создания подключения:
+//	{
+//	  "example": {
+//	    "filename":"test.txt",
+//	    "maxsize":2,
+//	    "maxage":22,
+//	    "maxbackups":5,
+//	    "localtime":true,
+//	    "compress":true,
+//	  }
+//	}
+
+//Пример вызова
+//	logg := Create("example")
+func Create(configPath) *Logger {
+	logg := new(Logger)
+	logg.init()
+	return logg
+}
+
+func (base *Logger) Info(format string, a ...interface{}) {
+	text := fmt.Sprintf(format, a)
+	base.write("INFO", text)
+}
+func (base *Logger) Trace(format string, a ...interface{}) {
+	text := fmt.Sprintf(format, a)
+	base.write("TRACE", text)
+}
+func (base *Logger) Debug(format string, a ...interface{}) {
+	text := fmt.Sprintf(format, a)
+	base.write("DEBUG", text)
+}
+func (base *Logger) Error(format string, a ...interface{}) {
+	text := fmt.Sprintf(format, a)
+	base.write("ERROR", text)
+}
+func (base *Logger) Panic(format string, a ...interface{}) {
+	text := fmt.Sprintf(format, a)
+	base.write("PANIC", text)
+}
+
+//инициализация логгера, если задание параметров проводилось в ручную
+func (base *Logger) init() {
+
+	base.logget = lumberjack.Logger{
+		Filename:   base.Config.Filename,
+		MaxSize:    base.Config.MaxSize,
+		MaxBackups: base.Config.MaxBackups,
+		MaxAge:     base.Config.MaxAge,
+		Compress:   base.Config.Compress,
+	}
+	base.isInit = true
+}
+
+//Запись в лог по определенному стандарту
+func (base *Logger) write(mode string, text string) {
+	if !base.isInit {
+		base.init()
+	}
+	base.logger.Write([]byte(fmt.Sprintf("%v %6v: %v\n", time.Now().Format("2006-01-02T15:04:05.999999-07:00"), text)))
+}
---
+++ type.go
@@ -0,0 +1,22 @@
+package logger
+
+import (
+	lumberjack "github.com/natefinch/lumberjack"
+)
+
+//Объект настройки логгера
+type LoggerConfig struct {
+	Filename   string `json:"filename"`   //имя файла, в который будут писаться логи
+	MaxSize    int    `json:"maxsize"`    //(МБ) максимальный размер файла, в который будут писаться логи
+	MaxAge     int    `json:"maxage"`     //Кол-во дней, которое хранятся логи
+	MaxBackups int    `json:"maxbackups"` //Кол-во фалов логов, которые хранятся
+	LocalTime  bool   `json:"localtime"`  //Используется ли локальное время для формирования названия файлов
+	Compress   bool   `json:"compress"`   //Архивация старых логов
+}
+
+//Объект логгера
+type Logger struct {
+	logget *lumberjack.Logger
+	Config *LoggerConfig
+	isInit bool
+}
